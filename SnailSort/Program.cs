3e91a30b4820a63a353e61aae21b13a014369a1c
SM Pavel <xgamez_channel@mail.ru>
2024-01-26 19:17:25 +0300

    Добавьте файлы проекта.

---
+++ SnailSort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnailSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random(); // Создаем новый экземпляр класса Random
+
+            var array = new int[3, 3]; // Создаем двумерный массив размером 5 на 5 элементов типа int
+
+            for (var i = 0; i < 3; i++) // Цикл по строкам массива
+            {
+                for (var j = 0; j < 3; j++) // Цикл по столбцам массива
+                {
+                    array[i, j] = rnd.Next(1, 10 + 1); // Заполняем каждый элемент массива случайным числом от 1 до 10 включительно
+                }
+            }
+            WriteArray(array); // Вызываем метод WriteArray с аргументом array
+
+            // Вызов метода Snail и получение результата
+            List<int> result = Solution.Snail(array);
+
+            Console.WriteLine();
+            // Вывод результата
+            foreach (int i in result)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+
+        static int[,] WriteArray(int[,] array) // Объявляем статический метод WriteArray с двумерным массивом типа int[,] в качестве параметра и возвращаемым значением
+        {
+            for (int i = 0; i < array.GetLength(0); i++) // Цикл по строкам массива
+            {
+                for (int j = 0; j < array.GetLength(1); j++) // Цикл по столбцам массива
+                {
+                    Console.Write(array[i, j] + " "); // Выводим значение текущего элемента массива и пробел в стандартный выходной поток
+                }
+                Console.WriteLine(); // Переходим на новую строку в стандартном выходном потоке
+            }
+            return array; // Возвращаем двумерный массив array без изменений
+        }
+    }
+}
---
+++ SnailSort/Solution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnailSort
+{
+    using System.Collections.Generic;
+
+    public class Solution
+    {
+        public static List<int> Snail(int[,] array)
+        {
+            var result = new List<int>();
+            int n = array.GetLength(0);
+            int m = array.GetLength(1);
+
+            int top = 0;
+            int bottom = n - 1;
+            int left = 0;
+            int right = m - 1;
+
+            while (top <= bottom && left <= right)
+            {
+                for (int i = left; i <= right; i++)
+                {
+                    result.Add(array[top, i]);
+                }
+                top++;
+
+                for (int i = top; i <= bottom; i++)
+                {
+                    result.Add(array[i, right]);
+                }
+                right--;
+
+                if (top <= bottom)
+                {
+                    for (int i = right; i >= left; i--)
+                    {
+                        result.Add(array[bottom, i]);
+                    }
+                    bottom--;
+                }
+
+                if (left <= right)
+                {
+                    for (int i = bottom; i >= top; i--)
+                    {
+                        result.Add(array[i, left]);
+                    }
+                    left++;
+                }
+            }
+
+            return result;
+        }
+    }
+
+}
